cbc8879eace52764d37e36957b7893acc48adaaa
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-03-27 15:48:14 +0100

    update: add socket creation to client

--- client/main.cpp
+++ client/main.cpp
@@ -2,17 +2,31 @@
 
 #include <iostream>
 #include <string>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 int main() {
-    std::string ipAdress;
+    std::string ipAddress;
     int port;
     
     std::wcout << "Server ipv4: ";
-    std::cin >> ipAdress;
+    std::cin >> ipAddress;
     std::wcout << "Server port: ";
     std::cin >> port; 
-    
-    Networking();
+
+
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    serverAddress.sin_addr.s_addr = inet_addr(ipAddress.c_str());
+
+    if(connect(serverSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) == -1) {
+        helpers::Packet responseCode = helpers::receiveMessage(serverSocket);
+        Networking(serverSocket);
+    }
     
     return 0;
 }
