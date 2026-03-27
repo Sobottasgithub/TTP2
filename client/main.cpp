e8c706f7819b5cb59ca647a5886366284759cb49
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-03-27 14:35:22 +0100

    update: add Networking to client

--- client/main.cpp
+++ client/main.cpp
@@ -1,5 +1,18 @@
+#include "src/networking.h"
+
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Client! Hello World!" << std::endl;
+    std::string ipAdress;
+    int port;
+    
+    std::wcout << "Server ipv4: ";
+    std::cin >> ipAdress;
+    std::wcout << "Server port: ";
+    std::cin >> port; 
+    
+    Networking();
+    
+    return 0;
 }
