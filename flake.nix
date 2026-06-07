30740e684e65c1a82a9daac325dbf9f3bcf6d13a
Patrick Schulze <patrick.schulze.ofp@gmail.com>
2026-06-07 15:27:34 +0200

    update: version in flake

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
-      version = "1.9.2";
+      version = "1.9.3";
 
       commonDeps = with pkgs; [
         cmake
