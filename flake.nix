d2044ab26f7d7a986649d57ac8f471da3a57d7f0
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-09-12 10:37:31 +0200

    update: version 2.2.5 >> 2.2.6

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
-      version = "2.2.5";
+      version = "2.2.6";
 
       libtablog = tablog.packages.${system}.lib;
 
